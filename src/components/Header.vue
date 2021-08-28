e50c468f7e6813c7251393277472fd5d1082e924
lavertis <29688908+Lavertis@users.noreply.github.com>
2021-08-28 19:37:23 +0200

    Initial commit

---
+++ src/components/Header.vue
@@ -0,0 +1,37 @@
+<template>
+  <header>
+    <h1>{{ title }}</h1>
+    <Button
+        v-show="homepage"
+        :color="showAddTask?'red':'green'"
+        :text="showAddTask?'Close':'Add Task'"
+        @btn-click="$emit('toggle-add-task')"/>
+  </header>
+</template>
+
+<script>
+import Button from "@/components/Button";
+
+export default {
+  name: "Header",
+  props: {
+    title: String,
+    showAddTask: Boolean,
+  },
+  components: {Button},
+  computed: {
+    homepage() {
+      return this.$route.path === '/';
+    }
+  }
+}
+</script>
+
+<style scoped>
+header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+}
+</style>
